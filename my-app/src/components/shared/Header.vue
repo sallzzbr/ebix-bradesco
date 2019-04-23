686879cb177f06a9601ff75eed6fddad8d1af4f0
Antonio Salgado <antoniosalgado@Antonios-MacBook-Pro.local>
2019-04-23 17:03:30 -0300

    header is done

---
+++ my-app/src/components/shared/Header.vue
@@ -0,0 +1,30 @@
+<template>
+    <v-toolbar>
+            <img src="../../assets/bradeslogo.png" alt="avatar" class="logo">
+            <v-spacer></v-spacer>
+            <p class="hidden-xs-only">Beltrano Silva de Souza</p>
+            <v-avatar class="blue-grey lighten-4" size="38px">
+                <img src="../../assets/user.png" onerror="../../assets/user.png">
+            </v-avatar>
+
+    </v-toolbar>
+</template>
+
+<script>
+export default {
+  name: 'Header'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.logo {
+    max-height: 60px;
+    max-width: 20vw;
+}
+
+p {
+    margin: 0 15px 0 0;
+}
+
+</style>
